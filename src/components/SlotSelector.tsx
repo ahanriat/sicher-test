6c5ce66f1bf1291dc357259f3b927804c85c8f18
ahanriat <ahanriat@gmail.com>
2020-03-22 15:46:38 +0100

    Fix SlotSelector height

--- src/components/SlotSelector.tsx
+++ src/components/SlotSelector.tsx
@@ -1,9 +1,16 @@
-import SecondaryText from 'components/SecondaryText';
-import * as React from 'react';
-import { Text, StyleProp, ViewStyle, View, Image, TextStyle } from 'react-native';
-import { Colors } from 'services/Colors';
-import Gradient from './Gradient';
-import TouchableScale from './TouchableScale';
+import SecondaryText from "components/SecondaryText";
+import * as React from "react";
+import {
+  Text,
+  StyleProp,
+  ViewStyle,
+  View,
+  Image,
+  TextStyle
+} from "react-native";
+import { Colors } from "services/Colors";
+import Gradient from "./Gradient";
+import TouchableScale from "./TouchableScale";
 
 export default function SlotSelector(props: ButtonProps) {
   return (
@@ -13,37 +20,50 @@ export default function SlotSelector(props: ButtonProps) {
       style={[
         props.style,
         {
-          overflow: 'visible'
+          overflow: "visible"
         }
       ]}
     >
       <Gradient
-        colors={props.selected && ['#35F285', '#6FCF97']}
+        colors={props.selected && ["#35F285", "#6FCF97"]}
         style={{
           borderRadius: 24,
           height: 48,
-          justifyContent: 'center'
+          justifyContent: "center"
         }}
       >
         <View
           style={{
-            backgroundColor: props.selected ? '' : 'white',
+            backgroundColor: props.selected ? "" : "white",
             paddingVertical: 12,
             paddingLeft: props.selected ? 11 : 24,
             paddingRight: 24,
             marginHorizontal: 2,
             borderRadius: 22,
-            flexDirection: 'row',
-            justifyContent: 'space-between'
+            height: 44,
+            flexDirection: "row",
+            justifyContent: "space-between"
           }}
         >
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            {props.selected &&
-            <Image source={require('../../assets/icons/green-checkbox.png')} style={{ marginRight: 20 }} />
-            }
-            <SecondaryText text={props.label} style={[props.selected && { color: Colors.white }, props.textStyle]}/>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            {props.selected && (
+              <Image
+                source={require("../../assets/icons/green-checkbox.png")}
+                style={{ marginRight: 20 }}
+              />
+            )}
+            <SecondaryText
+              text={props.label}
+              style={[
+                props.selected && { color: Colors.white },
+                props.textStyle
+              ]}
+            />
           </View>
-          <SecondaryText text={props.labelRight} style={props.selected && { color: Colors.white }}/>
+          <SecondaryText
+            text={props.labelRight}
+            style={props.selected && { color: Colors.white }}
+          />
         </View>
       </Gradient>
     </TouchableScale>
@@ -52,7 +72,7 @@ export default function SlotSelector(props: ButtonProps) {
 
 interface ButtonProps {
   label: string;
-  labelRight?: string
+  labelRight?: string;
   icon?: unknown;
 
   onPress(): void;
